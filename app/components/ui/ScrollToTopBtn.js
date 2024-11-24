d1fd8e97b3ee8eea5bc713d7c9424014e4d4b836
Elle Frankenberg <eleonor.frankenberg@gmail.com>
2024-11-24 19:08:18 +0100

    prevent hover effect on mobile

--- app/components/ui/ScrollToTopBtn.js
+++ app/components/ui/ScrollToTopBtn.js
@@ -3,14 +3,14 @@ import { FaArrowUp } from "react-icons/fa";
 const ScrollToTopBtn = ({ handleScrollToTop, showOnLargeScreen }) => {
   return (
     <button
-      className={`cursor-pointer flex items-center gap-2 py-[0.3rem] px-3   rounded-full  transition-colors hover:text-darkBlue ${
+      className={`cursor-pointer flex items-center gap-2 py-[0.3rem] px-3 text-darkGray rounded-full touch-none:hover:text-darkGray transition-colors hover:text-darkBlue ${
         showOnLargeScreen === false ? "lg:sr-only" : ""
       }`}
       aria-label="Scroll to top"
       onClick={handleScrollToTop}
     >
       <span>Scroll to top</span>
-      <FaArrowUp className="w-[20px] h-[20px]" aria-hidden="true" />
+      <FaArrowUp className="w-[20px] h-[20px] " aria-hidden="true" />
     </button>
   );
 };
